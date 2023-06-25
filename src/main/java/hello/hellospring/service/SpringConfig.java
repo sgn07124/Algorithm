d9180e078727b08d4aa5087af3d3e553465e0c98
Kim Kyeongho <sgn07124@gmail.com>
2023-06-25 17:15:12 +0900

    section6 - JDBC

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,12 +1,23 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;  // 1
+
+    @Autowired  // 2
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();  // db가 정해지지 않은 경우
+        // 아래의 문장 추가하고 위 1, 2 추가하면 됨
+        return new JdbcMemberRepository(dataSource);  // db가 정해져 연결
     }
 }
