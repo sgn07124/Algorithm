0e7a181671fed59b9332e93ff2b01dd21795b4fd
Kim Kyeongho <sgn07124@gmail.com>
2023-06-26 22:34:04 +0900

    section6 - Spring Data JPA

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -17,24 +17,32 @@ public class SpringConfig {
         this.dataSource = dataSource;
     }
     */
-    private EntityManager em;
+    /*private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+    */
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    private final MemberRepository memberRepository;
+
+    @Autowired // 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();  // db가 정해지지 않은 경우
         // 아래의 문장 추가하고 위 1, 2 추가하면 됨
         //return new JdbcMemberRepository(dataSource);  // db가 정해져 연결
         //return new JdbcTemplateMemberRepository(dataSource);  // Jdbc 실행
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+//    }
 }
